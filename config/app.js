9ba640388a6317c4c6ac51e6b8c2cd4196c2975a
Mojtaba Sharif <mojtabasharif777@gmail.com>
2019-03-06 09:42:52 +0330

    fix error

--- config/app.js
+++ config/app.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const v1 = require('../routes/v1');
 const app = express();
+app.use(express.json());
 app.use('/v1', v1);
 module.exports = app;
